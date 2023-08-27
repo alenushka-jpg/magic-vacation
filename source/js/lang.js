de1f264a5c5f7263e2fb2d5b65e7cf81a4f941ce
alenushka-jpg <alionxlo@gmail.com>
2023-08-27 11:32:02 +0300

    Adds multi-language support for promo and footer

--- source/js/lang.js
+++ source/js/lang.js
@@ -12,12 +12,10 @@ const langArr = {
       en: `Prizes`,
       ru: `Призы`
     },
-  },
-  change: {
-    en: `EN`,
-    ru: `RU`
-  },
-  media: {
+    change: {
+      en: `EN`,
+      ru: `RU`
+    },
     inst: {
       en: `Link to the Instagram social network`,
       ru: `Ссылка на социальную сеть инстаграм`
@@ -29,6 +27,50 @@ const langArr = {
     vk: {
       en: `Link to the VKontakte social network`,
       ru: `Ссылка на социальную сеть вконтакте`
+    },
+  },
+  promo: {
+    h1: {
+      en: `Contest "Mysterious Vacation": Win a trip to any country!`,
+      ru: `Конкурс "Таинственный отпуск": выиграйте путешествие в любую страну!`
+    },
+    h2: {
+      en: `Mysterious Vacation`,
+      ru: `Таинственный отпуск`
+    },
+    label: {
+      en: `Contest Duration:`,
+      ru: `Даты проведения конкурса:`
+    },
+    date: {
+      en: `October 1st - 31st, 2023`,
+      ru: `1 - 31.10 / 2023`
+    },
+    message: {
+      en:
+      `Be the first to decipher the destination and set off on an adventure
+      at your convenience, or seize one of the many available prizes.`,
+      ru:
+      `Отгадай первым место назначения и отправься в путешествие в любое время
+      или получи один из многочисленных призов.`
+    },
+  },
+  footer: {
+    showmess: {
+      en: `Show message`,
+      ru: `Показать сообщение`
+    },
+    hidemess: {
+      en: `Hide message`,
+      ru: `Скрыть сообщение`
+    },
+    label: {
+      en: `Contest Duration:`,
+      ru: `Даты проведения конкурса:`
+    },
+    date: {
+      en: `October 1st - 31st, 2023`,
+      ru: `1 - 31.10 / 2023`
     },
   },
 };
