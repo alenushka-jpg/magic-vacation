3722541a6e3b32cead1014eeb871e63f778dbc40
alenushka-jpg <alionxlo@gmail.com>
2023-08-27 16:17:18 +0300

    Adds multilingual content object for James Target's story

--- source/js/lang.js
+++ source/js/lang.js
@@ -73,4 +73,66 @@ const langArr = {
       ru: `1 - 31.10 / 2023`
     },
   },
+  slider: [
+    {
+      en: `James Target was the founder of "Fjord Inc". Since 1965,
+      "Fjord Inc" has been renowned for its reliable and convenient
+      suitcases and tourist accessories.`,
+      ru: `Джеймс Таргет был основателем компании "Fjord Inc".
+      С 1965 года "Fjord Inc" славилась своими надежными и
+      удобными чемоданами и аксессуарами для туристов.`
+    },
+    {
+      en: `When James was 7 years old, an uncle visited their home and
+      entertained the children with stories of his trip to a place
+      that seemed incredible to young Target. Then James decided
+      that he would visit it one day, but would not talk about the dream
+      until it came true.`,
+      ru: `Когда Джеймсу было 7 лет, дядя приезжал к ним домой и
+      рассказывал детям истории о своем путешествии в место,
+      которое казалось маленькому Таргету удивительным.
+      Тогда Джеймс решил, что однажды посетит это место,
+      но не будет рассказывать о своей мечте, пока она не сбудется.`
+    },
+    {
+      en: `James Target became an avid traveler.
+      He visited 5 continents and 126 countries.`,
+      ru: `Джеймс Таргет стал страстным путешественником.
+      Он посетил 5 континентов и 126 стран.`
+    },
+    {
+      en: `He rafted the Amazon and dived into the caves of France,
+      meditated in the mountains of Tibet and shook hands with the leader of the Akugaua tribe.`,
+      ru: `Он сплавлялся по Амазонке и нырял в пещеры Франции,
+      медитировал в горах Тибета и пожимал руку вождю племени Акугауа.`
+    },
+    {
+      en: `From his trips, James Target brought ideas on how to make his "Fjord Inc"
+      suitcases convenient and adapted to different travel conditions.`,
+      ru: `Из своих путешествий Джеймс Таргет привозил идеи о том,
+      как сделать свои чемоданы "Fjord Inc" удобными и приспособленными
+      для различных условий путешествия.`
+    },
+    {
+      en: `All this time, James never abandoned the idea of his dream trip. However,
+      as soon as he started planning, obstacles arose that prevented him from going there.`,
+      ru: `Все это время Джеймс не оставлял идею своего мечтаемого путешествия.
+      Однако, как только он начинал планировать, возникали препятствия,
+      которые мешали ему отправиться туда.`
+    },
+    {
+      en: `At the age of 67, James Target was caught in an avalanche and died.
+      In his will, he entrusted the adventurous journey to three brave souls,
+      who shared his spirit.`,
+      ru: `В возрасте 67 лет Джеймс Таргет попал под лавину и погиб.
+      В своем завещании он поручил отправиться в авантюрное путешествие
+      троим смелым душам, которые разделяли его дух.`
+    },
+    {
+      en: `Those who can identify the mysterious place by asking questions to the specially trained
+      AI "Sone", will embark on a fantastic journey.`,
+      ru: `Те, кто сможет узнать загадочное место, задавая вопросы специально
+      обученному ИИ "Соне", отправятся в фантастическое путешествие.`
+    },
+  ]
 };
