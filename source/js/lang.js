c426ecd8ba818c3ca722a48a00eeb56a2e28176e
alenushka-jpg <alionxlo@gmail.com>
2023-08-27 10:40:55 +0300

    Adds multi-language support for header

---
+++ source/js/lang.js
@@ -0,0 +1,34 @@
+const langArr = {
+  nav: {
+    home: {
+      en: `Home`,
+      ru: `Главная`
+    },
+    history: {
+      en: `History`,
+      ru: `История`
+    },
+    prizes: {
+      en: `Prizes`,
+      ru: `Призы`
+    },
+  },
+  change: {
+    en: `EN`,
+    ru: `RU`
+  },
+  media: {
+    inst: {
+      en: `Link to the Instagram social network`,
+      ru: `Ссылка на социальную сеть инстаграм`
+    },
+    fb: {
+      en: `Link to the Facebook social network`,
+      ru: `Ссылка на социальную сеть фейсбук`
+    },
+    vk: {
+      en: `Link to the VKontakte social network`,
+      ru: `Ссылка на социальную сеть вконтакте`
+    },
+  },
+};
