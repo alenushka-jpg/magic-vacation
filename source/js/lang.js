abf1a09174e6973d1a98014dd043d86859f7dd24
alenushka-jpg <alionxlo@gmail.com>
2023-08-27 17:09:09 +0300

    Adds multilanguage support for prizes section

--- source/js/lang.js
+++ source/js/lang.js
@@ -1,4 +1,15 @@
-const langArr = {
+const commonTranslations = {
+  contestDurationLabel: {
+    en: `Contest Duration:`,
+    ru: `Даты проведения конкурса:`
+  },
+  contestDate: {
+    en: `October 1st - 31st, 2023`,
+    ru: `1 - 31.10 / 2023`
+  }
+};
+
+const langPage = {
   nav: {
     home: {
       en: `Home`,
@@ -11,6 +22,14 @@ const langArr = {
     prizes: {
       en: `Prizes`,
       ru: `Призы`
+    },
+    rules: {
+      en: `Rules`,
+      ru: `Правила`
+    },
+    game: {
+      en: `Game`,
+      ru: `Игра`
     },
     change: {
       en: `EN`,
@@ -29,6 +48,9 @@ const langArr = {
       ru: `Ссылка на социальную сеть вконтакте`
     },
   },
+};
+
+const langPromo = {
   promo: {
     h1: {
       en: `Contest "Mysterious Vacation": Win a trip to any country!`,
@@ -38,14 +60,8 @@ const langArr = {
       en: `Mysterious Vacation`,
       ru: `Таинственный отпуск`
     },
-    label: {
-      en: `Contest Duration:`,
-      ru: `Даты проведения конкурса:`
-    },
-    date: {
-      en: `October 1st - 31st, 2023`,
-      ru: `1 - 31.10 / 2023`
-    },
+    label: commonTranslations.contestDurationLabel,
+    date: commonTranslations.contestDate,
     message: {
       en:
       `Be the first to decipher the destination and set off on an adventure
@@ -55,6 +71,9 @@ const langArr = {
       или получи один из многочисленных призов.`
     },
   },
+};
+
+const langFooter = {
   footer: {
     showmess: {
       en: `Show message`,
@@ -64,17 +83,20 @@ const langArr = {
       en: `Hide message`,
       ru: `Скрыть сообщение`
     },
-    label: {
-      en: `Contest Duration:`,
-      ru: `Даты проведения конкурса:`
-    },
-    date: {
-      en: `October 1st - 31st, 2023`,
-      ru: `1 - 31.10 / 2023`
-    },
+    label: commonTranslations.contestDurationLabel,
+    date: commonTranslations.contestDate,
   },
-  slider: [
-    {
+};
+
+const langSwiper = {
+  swiper: {
+    history: {
+      en: `History`,
+      ru: `История`
+    }
+  },
+  desc: {
+    1: {
       en: `James Target was the founder of "Fjord Inc". Since 1965,
       "Fjord Inc" has been renowned for its reliable and convenient
       suitcases and tourist accessories.`,
@@ -82,7 +104,7 @@ const langArr = {
       С 1965 года "Fjord Inc" славилась своими надежными и
       удобными чемоданами и аксессуарами для туристов.`
     },
-    {
+    2: {
       en: `When James was 7 years old, an uncle visited their home and
       entertained the children with stories of his trip to a place
       that seemed incredible to young Target. Then James decided
@@ -94,33 +116,33 @@ const langArr = {
       Тогда Джеймс решил, что однажды посетит это место,
       но не будет рассказывать о своей мечте, пока она не сбудется.`
     },
-    {
+    3: {
       en: `James Target became an avid traveler.
       He visited 5 continents and 126 countries.`,
       ru: `Джеймс Таргет стал страстным путешественником.
       Он посетил 5 континентов и 126 стран.`
     },
-    {
+    4: {
       en: `He rafted the Amazon and dived into the caves of France,
       meditated in the mountains of Tibet and shook hands with the leader of the Akugaua tribe.`,
       ru: `Он сплавлялся по Амазонке и нырял в пещеры Франции,
       медитировал в горах Тибета и пожимал руку вождю племени Акугауа.`
     },
-    {
+    5: {
       en: `From his trips, James Target brought ideas on how to make his "Fjord Inc"
       suitcases convenient and adapted to different travel conditions.`,
       ru: `Из своих путешествий Джеймс Таргет привозил идеи о том,
       как сделать свои чемоданы "Fjord Inc" удобными и приспособленными
       для различных условий путешествия.`
     },
-    {
+    6: {
       en: `All this time, James never abandoned the idea of his dream trip. However,
       as soon as he started planning, obstacles arose that prevented him from going there.`,
       ru: `Все это время Джеймс не оставлял идею своего мечтаемого путешествия.
       Однако, как только он начинал планировать, возникали препятствия,
       которые мешали ему отправиться туда.`
     },
-    {
+    7: {
       en: `At the age of 67, James Target was caught in an avalanche and died.
       In his will, he entrusted the adventurous journey to three brave souls,
       who shared his spirit.`,
@@ -128,11 +150,41 @@ const langArr = {
       В своем завещании он поручил отправиться в авантюрное путешествие
       троим смелым душам, которые разделяли его дух.`
     },
-    {
+    8: {
       en: `Those who can identify the mysterious place by asking questions to the specially trained
       AI "Sone", will embark on a fantastic journey.`,
       ru: `Те, кто сможет узнать загадочное место, задавая вопросы специально
       обученному ИИ "Соне", отправятся в фантастическое путешествие.`
     },
-  ]
+  }
 };
+
+const langPrizes = {
+  prizes: {
+    h3: {
+      en: `Prizes`,
+      ru: `Призы`
+    },
+    desc: {
+      en: `Find the answer and receive one of the prizes:`,
+      ru: `Найди ответ и получи один из призов:`
+    },
+    text: {
+      1: {
+        en: `mysterious trips`,
+        ru: `загадочных путешествия`
+      },
+      2: {
+        en: `reliable suitcases`,
+        ru: `надежных чемоданов`
+      },
+      3: {
+        en: `discount codes of 15%`,
+        ru: `промокодов на скидку 15%`
+      },
+    },
+  }
+};
+
+// eslint-disable-next-line no-console
+console.log(langPage, langPromo, langFooter, langSwiper, langPrizes);
